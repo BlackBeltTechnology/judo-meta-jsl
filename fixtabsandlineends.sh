f2cca3f83a9c1a1c45db4b7eeb5772ca69a20d17
gaborflorian <gabor.florian@blackbelt.hu>
2023-03-22 17:03:37 +0100

    Add the tabs replace in JslDslValidator.xtend

--- fixtabsandlineends.sh
+++
@@ -1,3 +0,0 @@
-#find . -type f \( -name "*.java" -or -name "*.eol" -or -name "*.etl" -or -name "*.xtend" \) -exec sed -i '' -e $'s/	/    /g' {} +
-find . -type f \( -name "*.java" -or -name "*.eol" -or -name "*.etl" -or -name "*.xtend" \) -exec sed -i '' -e "s/$(echo a | tr 'a' '\t')/    /g" {} +
-find . -type f \( -name "*.java" -or -name "*.eol" -or -name "*.etl" -or -name "*.xtend" \) -exec sed -i '' -e "s/[$(echo a | tr 'a' '\t') ]*$//g" {} +
