3e265bece7bb0854e1326138bba6f01af1a0e9bc
gprivi <>
2024-08-22 14:20:45 +0200

    JNG-5885 autocomplete

--- ide/ui/src/main/java/hu/blackbelt/judo/meta/jsl/ui/syntaxcoloring/HighlightingConfiguration.java
+++ ide/ui/src/main/java/hu/blackbelt/judo/meta/jsl/ui/syntaxcoloring/HighlightingConfiguration.java
@@ -1,9 +1,11 @@
+
+
 package hu.blackbelt.judo.meta.jsl.ui.syntaxcoloring;
 
-/*-
+/**-
  * #%L
  * Judo :: Jsl :: Model
- * %%
+ * * %%
  * Copyright (C) 2018 - 2022 BlackBelt Technology
  * %%
  * This program and the accompanying materials are made available under the
@@ -15,7 +17,7 @@ package hu.blackbelt.judo.meta.jsl.ui.syntaxcoloring;
  * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
  * with the GNU Classpath Exception which is
  * available at https://www.gnu.org/software/classpath/license.html.
- *
+ * @author gprivi
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  * #L%
  */
