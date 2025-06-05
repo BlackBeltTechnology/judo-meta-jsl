4e9e2f9b487171a325e4098196946fd4f58afd01
Robert Csakany <csakany.robert@gmail.com>
2025-06-05 21:10:36 +0200

    JNG-6310 Update epsilon (#186)
    [2025-06-05 19:43:45 +0200] JNG-6310 [Release] Updating versions
    [2025-06-04 22:05:56 +0200] Update versions
    [2025-06-04 14:45:02 +0200] JNG-6310 [Release] Updating versions
    [2025-06-04 14:17:26 +0200] JNG-6310 [Release] Updating versions
    [2025-06-04 10:13:12 +0200] JNG-6310 [Release] Updating versions
    [2025-06-04 03:03:05 +0200] JNG-6310 [Release] Updating versions
    [2025-06-03 15:54:52 +0200] Spring update
    [2025-06-02 11:06:46 +0200] JNG-6310 [Release] Updating versions
    [2025-06-02 09:36:12 +0200] Fixing Java 21 missing descriptors
    [2025-06-02 06:29:36 +0200] JNG-6310 [Release] Updating versions
    [2025-06-02 00:04:34 +0200] JNG-6310 [Release] Updating versions
    [2025-06-01 19:13:53 +0200] JNG-6310 Fixing Java 21 related versions
    [2025-05-29 04:46:32 +0200] JNG-6310 Fixes
    [2025-05-29 00:19:00 +0200] JNG-6310 Update epsilon

--- generator-maven-plugin-test/src/main/java/hu/blackbelt/jsl/archetype/maven/plugin/test/TestHelper.java
+++ generator-maven-plugin-test/src/main/java/hu/blackbelt/jsl/archetype/maven/plugin/test/TestHelper.java
@@ -20,8 +20,8 @@ package hu.blackbelt.jsl.archetype.maven.plugin.test;
  * #L%
  */
 
-import com.github.jknack.handlebars.internal.lang3.StringUtils;
 import hu.blackbelt.judo.generator.commons.StaticMethodValueResolver;
+import hu.blackbelt.judo.generator.commons.StringHelper;
 import hu.blackbelt.judo.generator.commons.annotations.TemplateHelper;
 import hu.blackbelt.judo.meta.jsl.jsldsl.ActorDeclaration;
 import hu.blackbelt.judo.meta.jsl.jsldsl.ModelDeclaration;
@@ -69,13 +69,13 @@ public class TestHelper extends StaticMethodValueResolver {
             nameTokens.remove(0);
             nameTokens.remove(nameTokens.size() - 1);
             return nameTokens.stream()
-                    .map(s -> StringUtils.capitalize(
+                    .map(s -> StringHelper.firstToUpperCase(
                             stream(s.replaceAll("#", "::")
                                     .replaceAll("\\.", "::")
                                     .replaceAll("/", "::")
                                     .replaceAll("_", "::")
                                     .split("::"))
-                                    .map(t -> StringUtils.capitalize(t))
+                                    .map(t -> StringHelper.firstToUpperCase(t))
                                     .collect(Collectors.joining())
                     ))
                     .collect(Collectors.joining());
@@ -90,7 +90,7 @@ public class TestHelper extends StaticMethodValueResolver {
     public static String modelName(String fqName) {
         String[] splitted = fqName.split("::");
         return fqClass(stream(splitted)
-                .map(s -> StringUtils.capitalize(s))
+                .map(s -> StringHelper.firstToUpperCase(s))
                 .findFirst().get());
     }
 
@@ -101,7 +101,7 @@ public class TestHelper extends StaticMethodValueResolver {
                 .replaceAll("/", "::")
                 .replaceAll("_", "::")
                 .split("::"))
-                .map(s -> StringUtils.capitalize(s))
+                .map(s -> StringHelper.firstToUpperCase(s))
                 .collect(Collectors.joining());
     }
 
@@ -111,7 +111,7 @@ public class TestHelper extends StaticMethodValueResolver {
                 .replaceAll("/", "::")
                 .split("::"))
                 .skip(1)
-                .map(s -> StringUtils.capitalize(s))
+                .map(s -> StringHelper.firstToUpperCase(s))
                 .collect(Collectors.joining());
     }
 
